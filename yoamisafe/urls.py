7a4c0ffeb27ad7a35996798297e6fdb8871c95e3
Hector Sandoval <hesandova@gmail.com>
2014-11-01 12:20:28 -0700

    removed yoamisafe.config modifed yoamisafe.urls

--- yoamisafe/urls.py
+++ yoamisafe/urls.py
@@ -2,7 +2,7 @@ from yoamisafe import views
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
-    url(r'yoamisafe?username=(?P<username>\D+)&location=(?P<longitude>\D+);(?P<latitude>\D+)$', views.map, name='map'),
+    url(r'?username=(?P<username>\D+)&location=(?P<longitude>\D+);(?P<latitude>\D+)$', views.map, name='map'),
     url(r'/temp', views.temp, name='temp'),
 
 )
